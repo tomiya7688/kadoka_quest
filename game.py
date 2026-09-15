ddd9c961354ef2809aa6386c1860ae626db613c0
kadoka <tomiya3499@gmail.com>
2026-09-15 19:59:11 +0900

    feat: enable simulation facility in source game entrypoint

--- game.py
+++ game.py
@@ -3,7 +3,10 @@ import sys
 
 sys.path.insert(0, str(Path(__file__).resolve().parent / "src"))
 
-from kadoka_quest.apps.game import main
+from kadoka_quest.apps.game import KadokaQuest, main
+from kadoka_quest.apps.simulation_facility_hooks import install_simulation_facility_hooks
+
+install_simulation_facility_hooks(KadokaQuest)
 
 
 if __name__ == "__main__":
